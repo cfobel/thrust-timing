b25fd6b5f2a4546144010bbaf823de5feff9abb9
Christian Fobel <christian@fobel.net>
2014-10-11 11:24:20 -0400

    Add test fixtures for ex5p, tseng, clma...
     - Add test fixtures for MCNC and U of T net-lists.
     - Add `-S` and `-V` arguments to `tests/timing-draft.py`.  Usage:
       Usage:
    
           timing-draft.py [-h] [-S SAVE_PATH | -V VERIFY_PATH | -f]
                           net_file_namebase
    
           Compute arrival times for netlist.
    
           positional arguments:
             net_file_namebase
    
           optional arguments:
             -h, --help            show this help message and exit
             -S SAVE_PATH, --save-path SAVE_PATH
                                   Path to save results to.
             -V VERIFY_PATH, --verify-path VERIFY_PATH
                                   Path containing data to verify against.
             -f, --overwrite

--- tests/timing-draft.py
+++ tests/timing-draft.py
@@ -1,7 +1,10 @@
 # coding: utf-8
+import re
 import sys
 import cPickle as pickle
 
+import pandas as pd
+from nose.tools import ok_
 from cyplace_experiments.data import open_netlists_h5f
 from path_helpers import path
 
@@ -25,7 +28,16 @@ from move_pair_thrust.delay_model import DelayModel
 
 
 @profile
-def _test_arrival_times(netlist_namebase):
+def _test_arrival_times(netlist_namebase, verify_file=None, save_file=None,
+                        append=False):
+    if verify_file is not None and save_file is not None:
+        raise RuntimeError('Cannot save result and verify result in the same '
+                           'run.  Please provide only one or the other.')
+    if verify_file is not None:
+        assert(verify_file.isfile())
+    if save_file is not None and save_file.isfile() and not append:
+        raise RuntimeError('Output file exists.  Specify `-a` to append.')
+
     netlist_h5f = open_netlists_h5f()
 
     netlist_group = getattr(netlist_h5f.root.netlists, netlist_namebase)
@@ -42,19 +54,38 @@ def _test_arrival_times(netlist_namebase):
     # connection, and using delay_model.clocked_sink_block_keys)
     arrival_times = delay_model.compute_arrival_times(netlist_namebase)
 
-    cached = path('%s-arrival_times.pickled' % netlist_namebase)
-    if cached.isfile():
-        data = pickle.load(cached.open('rb'))
-        assert((data == arrival_times).all())
+    if verify_file is not None:
+        data = pd.HDFStore(str(verify_file), 'r')
+        assert((getattr(data.root.arrival_times, netlist_namebase).values[:] ==
+                arrival_times).all())
+        data.close()
         print 'verified'
+    elif save_file is not None:
+        pd.Series(arrival_times).to_hdf(str(save_file), '/arrival_times/%s' %
+                                        netlist_namebase, complevel=2,
+                                        complib='blosc')
+        print 'wrote results to: %s' % save_file
     return adjacency_list, delay_model, arrival_times
 
 
-def test_arrival_times():
+def test_arrival_times_quick():
+    fixtures_path = path(__file__).parent.joinpath('fixtures',
+                                                   'arrival_times.dat')
     netlists = ['clma', 'ex5p', 'tseng', ]
 
     for n in netlists:
-        yield _test_arrival_times, n
+        yield _test_arrival_times, n, fixtures_path
+
+
+def test_arrival_times_full():
+    fixtures_path = path(__file__).parent.joinpath('fixtures',
+                                                   'arrival_times.dat')
+
+    h5f = pd.HDFStore(str(fixtures_path), 'r')
+    names = h5f.root.arrival_times._v_children.keys()
+    h5f.close()
+    for name in names:
+        yield _test_arrival_times, name, fixtures_path
 
 
 def parse_args(argv=None):
@@ -66,6 +97,12 @@ def parse_args(argv=None):
 
     parser = ArgumentParser(description='Compute arrival times for netlist.')
     parser.add_argument(dest='net_file_namebase')
+    result_options = parser.add_mutually_exclusive_group()
+    result_options.add_argument('-S', '--save-path', type=path, help='Path '
+                                'to save results to.')
+    result_options.add_argument('-V', '--verify-path', type=path, help='Path '
+                                'containing data to verify against.')
+    parser.add_argument('-a', '--append', action='store_true')
 
     args = parser.parse_args()
     return args
@@ -74,4 +111,5 @@ def parse_args(argv=None):
 if __name__ == '__main__':
     args = parse_args()
     adjacency_list, delay_model, arrival_times = _test_arrival_times(
-        args.net_file_namebase)
+        args.net_file_namebase, verify_file=args.verify_path,
+        save_file=args.save_path, append=args.append)
